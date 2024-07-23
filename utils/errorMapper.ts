feb4fad4ac4a68b6d7360a87181c11cf93dd6ade
52120776-b7f2-42db-9055-ac87e9518dd1 <4a418978-52cc-4042-8878-842d22fa0354.parachute080@passfwd.com>
2024-07-23 16:49:25 +0300

    errorMapper type fix

--- utils/errorMapper.ts
+++ utils/errorMapper.ts
@@ -5,7 +5,9 @@ type Result = Record<string, string>
 export const errorMapper = (errors: FieldError[]): Result => {
   const result: Result = {}
   errors.forEach(({ field, message }) => {
-    result[field] = message
+    if (field) {
+      result[field] = message
+    }
   })
   return result
 }
